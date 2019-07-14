5ee83dea54d2d00593ba2b4ee11b006ddd6046d0
mjhorvath <17457030+mjhorvath@users.noreply.github.com>
2019-07-14 06:20:49 -0400

    Update pov2ldr.js

--- pov2ldr.js
+++ pov2ldr.js
@@ -1,5 +1,21 @@
-// This file is licensed under the terms of the LGPL.
-var program_version = 'pov2ldr 0.5.0 beta\nMichael Horvath <mhorvath2161_at_juno_dot_com>\nThis file is licensed under the terms of the LGPL.'
+// pov2ldr 0.5.0 beta
+// Copyright (C) 2008  Michael Horvath
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+var program_version = 'pov2ldr 0.5.0 beta\nMichael Horvath\nThis file is licensed under the terms of the LGPL.'
 var program_args = WScript.Arguments
 var WshShell = WScript.CreateObject('WScript.Shell')
 var fso = new ActiveXObject('Scripting.FileSystemObject')
